0f0ee95ccfc32c6e78f70dcb858779fc3877e65d
VKutnyakov <vkutnyakov@guardian.com>
2019-05-15 15:54:36 +0300

    #1 Modal form test example

--- src/main/java/com/vladmirk/transkontservice/HomeController.java
+++ src/main/java/com/vladmirk/transkontservice/HomeController.java
@@ -1,19 +1,24 @@
 package com.vladmirk.transkontservice;
 
+import com.vladmirk.transkontservice.party.Expeditor;
+import com.vladmirk.transkontservice.party.PartyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 
+  private PartyService partyService;
   @Autowired
-  public HomeController() {
+  public HomeController(PartyService partyService) {
 
+    this.partyService = partyService;
   }
 
   @GetMapping("/login")
@@ -36,6 +41,20 @@ public class HomeController {
   public String testForm(@RequestParam("suggestTest") String suggest, ModelAndView model) {
 
     return "index";
+  }
+
+
+  // Test
+  @GetMapping("/testModal")
+  public String testModal() {
+    return "modalForm";
+  }
+
+  @PostMapping("/testModal")
+  @ResponseBody
+  public Expeditor postModal(@RequestParam("someModal") String s) {
+    Expeditor expeditor = partyService.findExpeditorByCode("00-00006612");
+    return expeditor;
   }
 
 }
